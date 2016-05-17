9838aab3b48bac667b2cc89ddcb2d50a78d015d0
brian <brn.j.williams@gmail.com>
2016-05-17 12:52:55 -0500

    update

--- bin/convertFitsTemplate.js
+++ bin/convertFitsTemplate.js
@@ -15,15 +15,22 @@ importClass(Packages.ij.gui.GenericDialog);
 // Open images:
 
 imp = IJ.openImage("/home/brian/vnand/2_2v_12min.fits");
+imp.show();
 imp = IJ.openImage("/home/brian/vnand/2_2v_12min_X1.fits");
-imp = IJ.openImage("/home/brian/vnand/2_2v_12min_X2.fits");
-imp = IJ.openImage("/home/brian/vnand/2_2v_12min_X3.fits");
+imp.show();
+//imp3 = IJ.openImage("/home/brian/vnand/2_2v_12min_X2.fits");
+//imp3.show();
+//imp4 = IJ.openImage("/home/brian/vnand/2_2v_12min_X3.fits");
+//imp4.show();
 
 // Concatenate images:
 
 // Try following line with newlines between filenames and without; this may make sed scripts easier in bash:
-IJ.run("Concatenate...", "  title=[Concatenated Stacks] image1=2_2v_12min.fits image2=2_2v_12min_X1.fits image3=2_2v_12min_X2.fits image4=2_2v_12min_X3.fits image5=[-- None --]");
+IJ.run("Concatenate...", "  title=[Concatenated Stacks] image1=2_2v_12min.fits image2=2_2v_12min_X1.fits image3=[-- None --]");
 
+var imp = IJ.getImage();
+
+imp.show();
 // Convert to 16-bit imagetype:
 
 IJ.run(imp, "16-bit", "");
